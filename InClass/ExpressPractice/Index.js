88a2acdda74a84ed0dc52784d42cc4d9d641f1e9
Ruby Koval <rtkoval42@tntech.edu>
2024-03-25 17:41:59 -0500

    In class Database work

--- InClass/ExpressPractice/Index.js
+++ InClass/ExpressPractice/Index.js
@@ -1,66 +1,100 @@
 const express = require('express');
 const cors = require('cors');
-
-const HTTP_PORT = 8000; // Create a port ; 80 is reserved for http, you cant use 80. If unsure look up a list of commonly used ports
-console.log("Listening on port " + HTTP_PORT);// 
+const sqlite3 = require('sqlite3').verbose();
+const dbSource = "node.db";
+const db = new sqlite3.Database(dbSource);
+const HTTP_PORT = 8000;
+console.log("Listening on port " + HTTP_PORT);
 var app = express();
-app.use(cors()); //cross origin resource sharing-  "look where the request is coming from"
-class Fruit{
-    constructor(strName, strColor){
-        this.name = strName;
-        this.color=strColor;
+app.use(cors());
 
+class Fruit {
+    constructor(strName,strColor){
+        this.name = strName;
+        this.color = strColor;
     }
 }
 
-var arrFruit = []; // array of objects 
-let objBanana = new Fruit('banana', 'yellow')
-let objApple = new Fruit('apple', 'red');
-let objGrape = new Fruit('grape', 'purple');
+var arrFruit = [];
+let objBanana = new Fruit('banana','yellow');
+let objApple = new Fruit('apple','red');
+let objGrape = new Fruit('grape','purple');
 arrFruit.push(objBanana);
 arrFruit.push(objApple);
-arrFruit.push(objGrape); // Pushed into the array of fruits "Pick it up, and push it onto the trailer"
-arrFruit.push(new Fruit('kiwi', 'brown'))
-app.get("/fruit"(req,res,next))=>{
-    let strName=req.query.name;
-    if (strName){
-        arrFruit()
-        if (fruit.name == strName){
+arrFruit.push(objGrape);
+arrFruit.push(new Fruit('kiwi','brown'));
+app.get("/", (req,res,next) => {
+    res.status(200).send(arrFruit);
+})
 
-        }
-        res.status(200).send(arrFruit)
+app.post("/fruit",(req,res,next) => {
+    let strName = req.query.name;
+    let strColor = req.query.color;
+    let strCommand = "INSERT INTO tblFruit VALUES(?,?)";
+    if(strName && strColor){
+        let arrParameters = [strName,strColor];
+        let objFruit = new Fruit(strName, strColor);
+        db.run(strCommand,arrParameters, function(err,result){
+            if(err){
+                res.status(400).json({error:err.message})
+            } else {
+                res.status(201).json({
+                    message:"success",
+                    fruit:objFruit
+                })
+            }
+        })
+    } else {
+        res.status(400).json({error:"Not all parameters provided"})
     }
-    else{
-        res.status(200).send(arrFruit)
+})
+
+app.get("/fruit",(req,res,next) => {
+    let strName = req.query.name;
+    if(strName){
+        let strCommand = "SELECT * FROM tblFruit WHERE name = ?";
+        let arrParameters = [strName];
+        db.all(strCommand,arrParameters,(err,row) => {
+            if(err){
+                res.status(400).json({error:err.message});
+            } else {
+                if(row.length < 1){
+                    res.status(200).json({message:"error: not found"})
+                } else{
+                    res.status(200).json({message:"success",fruit:row})
+                }
+            }
+        })
+    } else {
+        res.status(400).json({error:"No fruit name provided"});
     }
+})
 
-}
-app.post("/fruit"(req,res,next))=>{
-    res.status(200).send(arrFruit))
-    let strName=req.query.name;
-    let strColor=req.Fruit
-    arrFruit.push(new Fruit(strName, strFruit));
-    res.status(201).send(arrFruit);
-
-}
-app.get("/", (req,res,next) => { //HTTTP verb it will rspond with for restful api, / is BASE URL localhost 3000 
-    let strFruit = req.query.fruit; //
-    console.log("Routed to base route");
-    console.log(strFruit);
-    arrFruit.forEach(function(thisfruit){
-        if(thisfruit.name == strFruit){
-            res.status(200).send(arrFruit); //200 is okay ; ADDING A RECORD SUCCESSFULLY is 201, NOT FOUND is 404
-        }
-    })
-    res.status(200).send('{"message":"Fruit not found"}');
+app.delete("/fruit",(req,res,next) => {
+    let strName = req.query.name;
+    if(strName){
+        arrFruit.forEach(function(fruit,index){
+            if(fruit.name == strName){
+                arrFruit.splice(index,-1);
+                res.status(200).send(fruit);
+            }
+        })
+        res.status(200).send({message:'Fruit Not Found'});
+    } else {
+        arrFruit = [];
+        res.status(200).send(arrFruit);
+    }
 })
 
 app.get("/hello", (req,res,next) => {
-    let strFruit = req.query.fruit;//?fruit=banana EXAMPLE
-    console.log("Routed to hello route");
-    console.log(strFruit);
-   
-    res.status(200).send("Hello " + strFruit);
+    let strCommand = 'SELECT * FROM tblFruit';
+    db.all(strCommand,(err, row) => {
+        if(err){
+            res.status(400).json({error:err.message});
+        } else {
+            res.status(200).json({message:"success",fruit:row})
+        }
+    })
 })
 
 app.listen(HTTP_PORT);
